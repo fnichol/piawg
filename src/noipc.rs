7f4af0c9d29e2365b1c7c093b4cc55efff9b5fdf
Fletcher Nichol <fnichol@nichol.ca>
2021-09-14 15:46:02 -0600

    chore(noipc): add error handling & propagation
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- src/noipc.rs
+++ src/noipc.rs
@@ -2,42 +2,47 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::wg::{WgConfig, WgQuick};
+use crate::wg::{WGError, WgConfig, WgQuick};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum NoIpcError {
+    #[error("wireguard error")]
+    WG(#[from] WGError),
+}
 
 #[cfg(not(feature = "ipc"))]
+#[derive(Debug)]
 pub struct InterfaceManagerClient {}
 
 #[cfg(not(feature = "ipc"))]
 impl InterfaceManagerClient {
-    pub async fn start() -> Result<Self, Box<dyn std::error::Error>> {
+    pub async fn start() -> Result<Self, NoIpcError> {
         Ok(Self {})
     }
 
-    pub async fn wg_interface_up(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn wg_interface_up(&mut self) -> Result<(), NoIpcError> {
         WgQuick::for_existing_interface(crate::INTERFACE)?
             .up()
             .await
             .map_err(From::from)
     }
 
-    pub async fn wg_interface_down(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn wg_interface_down(&mut self) -> Result<(), NoIpcError> {
         WgQuick::for_existing_interface(crate::INTERFACE)?
             .down()
             .await
             .map_err(From::from)
     }
 
-    pub async fn write_wg_config(
-        &mut self,
-        config: WgConfig,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn write_wg_config(&mut self, config: WgConfig) -> Result<(), NoIpcError> {
         WgQuick::for_interface(crate::INTERFACE, &config)
             .await
             .map_err(From::from)
             .map(|_| ())
     }
 
-    pub async fn terminate(self) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn terminate(self) -> Result<(), NoIpcError> {
         Ok(())
     }
 }
